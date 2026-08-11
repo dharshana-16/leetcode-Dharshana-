14d398fdffa276719bba26e5bddd1a40a166dd20
dharshana-16 <dharshana85427@gmail.com>
2026-08-11 16:05:53 +0530

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.java
@@ -0,0 +1,21 @@
+// Last updated: 11/08/2026, 16:05:55
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        int c = 0;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(ch>='A' && ch <= 'Z'){
+                c++;
+            }
+        }
+        if(c==n){
+            return true;
+        }if(c==0){
+            return true;
+        }if(c==1 && word.charAt(0)>='A' && word.charAt(0)<='Z'){
+            return true;
+        }
+        return false;
+    }
+}
