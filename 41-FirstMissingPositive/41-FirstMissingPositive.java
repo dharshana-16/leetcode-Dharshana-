acc1eb7dab19f6ba2a6ddf5795f625bea89d9a19
dharshana-16 <dharshana85427@gmail.com>
2026-08-11 16:11:07 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -0,0 +1,18 @@
+// Last updated: 11/08/2026, 16:11:09
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n+1];
+        for(int num : nums){
+            if(num >0 && num <= n){
+                freq[num]=1;
+            }
+        }
+        for(int i=1; i<=n; i++){
+            if(freq[i]==0){
+                return i;
+            }
+        }
+        return n+1;
+    }
+}
