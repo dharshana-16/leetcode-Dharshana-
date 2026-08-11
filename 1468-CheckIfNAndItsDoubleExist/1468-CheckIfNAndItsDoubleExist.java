e91b31eee1041822787b4d74d67822bcbe631366
dharshana-16 <dharshana85427@gmail.com>
2026-08-11 16:02:07 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,17 @@
+// Last updated: 11/08/2026, 16:02:08
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n=arr.length;
+        Map<Integer, Integer> map = new HashMap();
+        for(int i=0; i<n; i++){
+            if(map.containsKey(arr[i]*2)){
+                return true;
+            }
+            else if(arr[i]%2==0 && map.containsKey(arr[i]/2)){
+                return true;
+            }
+            map.put(arr[i],map.getOrDefault(arr[i],0+1));
+        }
+        return false;
+    }
+}
