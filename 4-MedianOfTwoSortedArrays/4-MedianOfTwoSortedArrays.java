31a554bdaa4c721e167631dc80564f2afa3eacbc
dharshana-16 <dharshana85427@gmail.com>
2026-08-11 16:12:28 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.java
@@ -0,0 +1,44 @@
+// Last updated: 11/08/2026, 16:12:29
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = 0, right = m;
+        while (left <= right) {
+            int partition1 = (left + right) / 2;
+            int partition2 = (m + n + 1) / 2 - partition1;
+            int maxLeft1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
+            int minRight1 = (partition1 == m) ? Integer.MAX_VALUE : nums1[partition1];
+
+            int maxLeft2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
+            int minRight2 = (partition2 == n) ? Integer.MAX_VALUE : nums2[partition2];
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                if ((m + n) % 2 != 0) {
+                    return Math.max(maxLeft1, maxLeft2);
+                } else {
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+                }
+            } else if (maxLeft1 > minRight2) {
+                right = partition1 - 1; 
+            } else {
+                left = partition1 + 1; 
+            }
+        }
+
+        throw new IllegalArgumentException("Input arrays are not sorted.");
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2));  
+        int[] nums1_2 = {1, 2};
+        int[] nums2_2 = {3, 4};
+        System.out.println(solution.findMedianSortedArrays(nums1_2, nums2_2)); 
+    }
+}
