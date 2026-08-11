410e57fc224fa745a2b1eadbe043f1f48077356c
dharshana-16 <dharshana85427@gmail.com>
2026-08-11 16:01:12 +0530

    Create 2847-FindMaximumNumberOfStringPairs

---
+++ 2847-FindMaximumNumberOfStringPairs/2847-FindMaximumNumberOfStringPairs.java
@@ -0,0 +1,13 @@
+// Last updated: 11/08/2026, 16:01:13
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        int ans=0;
+        for(int i=0;i<words.length;i++) {
+        	for(int j=i+1;j<words.length;j++) {
+        		if(words[i].charAt(0)==words[j].charAt(1) && words[i].charAt(1)==words[j].charAt(0)) 
+        			ans++;
+        	}
+        }
+        return ans;
+    }
+}
