a7a6b766652dbd2e42178f1299b2de65527ea414
dharshana-16 <dharshana85427@gmail.com>
2026-08-11 16:05:29 +0530

    Create 586-CustomerPlacingTheLargestNumberOfOrders

---
+++ 586-CustomerPlacingTheLargestNumberOfOrders/586-CustomerPlacingTheLargestNumberOfOrders.sql
@@ -0,0 +1,7 @@
+-- Last updated: 11/08/2026, 16:05:30
+# Write your MySQL query statement below
+SELECT customer_number
+FROM Orders 
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1;
